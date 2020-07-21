893c5cb023f1194de933e99d5caf449ef0f847c6
artegoser <59178854+artegoser@users.noreply.github.com>
2020-07-21 16:17:33 +0300

    tardis update to json

--- tardis2.0.py
+++ tardis2.0.py
@@ -5,7 +5,7 @@ import os
 import random
 import json
 
-clear = "TARDIS------TARDIS------TARDIS------TARDIS--------artegoserdev---------------------------------------------------------\n" * 10000
+clear = "TARDIS------TARDIS------TARDIS------TARDIS--------artegoserdev---------------------------------------------------------\n"
 print(clear)
 vopr = 1
 otv = 1
@@ -19,104 +19,6 @@ temp=0
 after_id = ''
 
 
-
-#def speak(what):
- # if otv == 0:
-  # if golos == 0:
-   # try:
-    # tts = gTTS(text=what, lang="ru")
-     #tts.save(what +'.mp3')
-#     mixer.music.load(what +'.mp3')
- #    print(what)
- #    mixer.music.play()
- #    while mixer.music.get_busy():
- #           time.sleep(0.1)
- #   except:
- #    mixer.music.load(what +'.mp3')
- #    print(what)
- #    mixer.music.play()
- #    while mixer.music.get_busy():
- #           time.sleep(0.1)
- #    pass
- # else:
- #   print(what)
- # else:
- #  if golos == 0:
- #   try:
- #    tts = gTTS(text=what, lang="ru")
- #    tts.save('message.mp3')
- #    mixer.music.load('message.mp3')
- #    print(what)
- #    mixer.music.play()
- #    while mixer.music.get_busy():
- #           time.sleep(0.1)
- #    try:
- #     path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'message1.mp3')
- #     os.remove(path)
- #    except:
- #     pass
- #   except:
- #    tts = gTTS(text=what, lang="ru")
- #    tts.save('message1.mp3')
- #    mixer.music.load('message1.mp3')
- #    print(what)
- #    mixer.music.play()
- #    while mixer.music.get_busy():
- #           time.sleep(0.1)
- #    try:
- #     path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'message.mp3')
- #     os.remove(path)
- #    except:
- #     pass
- #  else:
- #   print(what)
-#def speake(what):
-#  if otv == 0:
-#   if golos == 0:
-#    try:
-#     tts = gTTS(text=what, lang="ru")
-#     tts.save(what +'.mp3')
-#     mixer.music.load(what +'.mp3')
-#     mixer.music.play()
-#     while mixer.music.get_busy():
-#            time.sleep(0.1)
-#    except:
-#     mixer.music.load(what +'.mp3')
-#     mixer.music.play()
-#     while mixer.music.get_busy():
-#            time.sleep(0.1)
-#     pass
-#   else:
-#    pass
-#
-#  else:
-#   if golos == 0:
-#    try:
-#     tts = gTTS(text=what, lang="ru")
-#     tts.save('message.mp3')
-#     mixer.music.load('message.mp3')
-#     mixer.music.play()
-#     while mixer.music.get_busy():
-#            time.sleep(0.1)
-#     try:
-#      path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'message1.mp3')
-#      os.remove(path)
- #    except:
-  #    pass
-   # except:
-#     tts = gTTS(text=what, lang="ru")
- #    tts.save('message1.mp3')
-  #   mixer.music.load('message1.mp3')
-   #  mixer.music.play()
-#     while mixer.music.get_busy():
- #           time.sleep(0.1)
-  #   try:
-   #   path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'message.mp3')
-#      os.remove(path)
- #    except:
-  #    pass
-#   else:
- #   pass
 def xor_cipher( str, key ):
  encript_str = ""
  for letter in str:
@@ -127,5 +29,40 @@ def ciphergenkey():
     key = int(random.choice(cipherkey) + random.choice(cipherkey) + random.choice(cipherkey) + random.choice(cipherkey) + random.choice(cipherkey) + random.choice(cipherkey))
     speak("Ваш ключ: " + str(key))
 
-s = 1
+
 #speak("Вас приветствует ТАРДИС. Голосовой помощник.\nЕсли хотите узнать больше информации напишите Информация.\nДля изменения настроек напишите настройки.")
+
+
+
+#Считывание настроек
+
+try:
+    with open("options.tardis", "r", encoding = "utf-8") as options:
+        optionsdata = json.load(options)
+except:
+    optionsdata = {
+                "name": "Пользователь",   
+                "assname": "ТАРДИС"
+            }
+    with open("options.tardis", "w", encoding = "utf-8") as options:
+            json.dump(optionsdata, options)
+name = optionsdata["name"]
+assname = optionsdata["assname"]
+
+print("Здравствуйте, " + name + ". Меня зовут, " + assname + ". Я буду вашим компьютерным ассистентом")
+
+
+s = 1
+while s not in ["пока"]:
+    s = input()
+    if s in ["настройки", "настроить"]:
+        #Перезапись настроек
+
+        name = input("Ведите ваше имя: ")
+        assname = input("Назовите меня: ")
+        optionsdata = {
+                "name": name,   
+                "assname": assname
+            }
+        with open("options.tardis", "w", encoding = "utf-8") as options:
+            json.dump(optionsdata, options)
