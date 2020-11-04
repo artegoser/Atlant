8fd5a16e71bb36daebde3426ea650070b3d29fe5
PhlEEum <timofeev.evgeniy767@mail.ru>
2020-11-05 00:42:50 +0300

    кубики квадратики

--- modules/mathematicks.py
+++ modules/mathematicks.py
@@ -2,12 +2,10 @@ def fact(v):
     i = 1
     for factorial in range(1, v+1):
      i = i*factorial
-    return i
-
+    return i 
 def discriminant(a, b, c):
    D = int((b**2)-4*a*c)
    return D
-
 def diskRoots(a, b, c, D):
     if D > 0 :
      x1 = (-b + (D**0.5))/(2*a)
@@ -18,3 +16,21 @@ def diskRoots(a, b, c, D):
      return x
     else :
      return False
+def AreaRectangle(x, y):
+    return x*y
+def PerRectangleb(x, y):
+    return (2*x)*(2*y)
+def OCube (x):
+    # пириметр куба ,Общая длинна 
+    return 12*x
+def PCube (x):
+    #Площадь одной стороны
+    return x**2
+def QCube (x):
+    #Площадь куба(поверхность)
+    return 6*(a**2)
+def VCube (x):
+    #Объем куба
+    return x
+
+   
