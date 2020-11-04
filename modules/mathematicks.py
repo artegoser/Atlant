8f711f75812b827ce8f6bab0a1ae3e6ec8fbf280
PhlEEum <timofeev.evgeniy767@mail.ru>
2020-11-04 23:26:26 +0300

    починил 3.0

--- modules/mathematicks.py
+++ modules/mathematicks.py
@@ -13,8 +13,8 @@ def diskRoots(a, b, c, D):
      x1 = (-b + (D**0.5))/(2*a)
      x2 = (-b - (D**0.5))/(2*a)
      return x1, x2
-    elif == 0 :
-     x = -(b\(2*a))
+    elif D == 0 :
+     x = (-(b/(2*a)))
      return x
     else :
      return False
