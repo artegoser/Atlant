42423b36ac89feb1013ffa44947dd7f8e727e519
PhlEEum <timofeev.evgeniy767@mail.ru>
2020-11-04 22:56:55 +0300

    квадратное уравнение
    ПОПЫТКА

--- modules/mathematicks.py
+++ modules/mathematicks.py
@@ -7,3 +7,14 @@ def fact(v):
 def discriminant(a, b, c):
    D = int((b**2)-4*a*c)
    return D
+
+def discKorni(a, b, c, D):
+    if D > 0 :
+     x1 = (-b + (D**0.5))/2a 
+     x2 = (-b - (D**0.5))/2a
+     return x1, x2
+    elif == 0 :
+     x = -(b\2a)
+     return x
+    else 
+     return False
