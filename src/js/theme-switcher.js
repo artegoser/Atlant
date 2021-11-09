def59425aa57967852e64200061d576a725caa07
Artemy <artegoser@gmail.com>
2021-11-09 21:04:58 +0300

    fix theme switcher and locals

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,14 +1,18 @@
+let fs = require("fs");
+const { ipcRenderer } = require('electron')
+
 window.addEventListener("load", ()=>{
     let switcher = document.getElementById("theme-switcher");
     let theme = document.getElementById("theme");
-    if(localStorage.getItem("theme") === "light"){
-        theme.href = "../css/light.css";
-        switcher.checked = true;
-    } else{
-        theme.href = "../css/black.css";
-    }
+    if(localStorage.getItem("theme") === "light") switcher.checked = true;
+    else if(localStorage.getItem("theme") === "black") switcher.checked = false;
     switcher.onchange = ()=>{
         localStorage.setItem("theme", switcher.checked === true ? "light" : "black");
+        if(switcher.checked === true){
+            fs.writeFileSync("./src/css/theme.css", `@import "./light.css"`);
+        } else{
+            fs.writeFileSync("./src/css/theme.css", `@import "./black.css"`);
+        }
         theme.href = switcher.checked === true ? "../css/light.css" : "../css/black.css";
     }
 });
