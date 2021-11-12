cc39faf1b25855483d61d1c92e2adadf2d0e8f52
Artemy <artegoser@gmail.com>
2021-11-12 22:10:05 +0300

    show all types

--- src/js/gui-model.js
+++ src/js/gui-model.js
@@ -5,9 +5,16 @@ let dataset = JSON.parse(fs.readFileSync("./td-2.0.json", "utf-8"));
 compiler(manager, dataset);
 
 window.addEventListener("load", ()=>{
+    function updatetypes(){
+        let types = new Set(Object.keys(dataset.questions.ru).concat(Object.keys(dataset.questions.en)).concat(Object.keys(dataset.answers.en)).concat(Object.keys(dataset.answers.ru)));
+        document.getElementById("types").value = Array.from(types).join("\n");
+    }
+    updatetypes();
     document.getElementById("send").onclick = () => {
+        dataset[document.getElementById("typeofm").value][document.getElementById("lang").value][document.getElementById("type").value] = dataset[document.getElementById("typeofm").value][document.getElementById("lang").value][document.getElementById("type").value] || [];
         dataset[document.getElementById("typeofm").value][document.getElementById("lang").value][document.getElementById("type").value].push(document.getElementById("item").value)
         fs.writeFileSync("./td-2.0.json", JSON.stringify(dataset, null, "  "));
         compiler(manager, dataset);
+        updatetypes();
     }
 });
